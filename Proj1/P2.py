13af72075dc137de05e949279c93a651b77ade15
andylws <andylws@naver.com>
2021-04-01 23:00:21 +0900

    solved P2

--- Proj1/P2.py
+++ Proj1/P2.py
@@ -2,7 +2,7 @@
 **Instruction**
 <Factor into two prime numbers>
 Input of P2 function is natural number.
-P2 function returns whether the input nubmer could be factored into two prime numbers.
+P2 function returns whether the input number could be factored into two prime numbers.
 Do not worry about invalid input.
 
 >>> P2(6) #2 * 3
@@ -20,10 +20,29 @@ False
 
 """
 
-def P2(n:int) -> bool:        
+
+def P2(n: int) -> bool:
     ##### Write your Code Here #####
-    
+
+    def findDivisors(N):
+        divisor_list = []
+
+        for i in range(1, int(N + 1)):
+            if N % i == 0:
+                divisor_list.append(i)
+
+        return divisor_list
+
+    n_divisor_list = findDivisors(n)
+
+    for divisor in n_divisor_list:
+        couple = n / divisor
+        divisor_divisor = findDivisors(divisor)
+        couple_divisor = findDivisors(couple)
+        if len(divisor_divisor) == 2 and len(couple_divisor) == 2:
+            return True
+        else:
+            continue
+
     return False
     ##### End of your code #####
-
-
