e9838349ed2437049e2e20bb7f4b7427a8a75beb
andylws <andylws@naver.com>
2021-04-03 02:30:20 +0900

    solved P1 of HW7

--- HW7/P1.py
+++ HW7/P1.py
@@ -1,3 +1,15 @@
 def P1(lst):
+    current_swap = 1
+    total_swap = 0
 
-    return 
+    while current_swap != 0:
+        current_swap = 0
+        for i in range(len(lst) - 1):
+            if lst[i] > lst[i + 1]:
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
+                total_swap = total_swap + 1
+                current_swap = current_swap + 1
+
+    return total_swap
