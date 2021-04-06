9b66759a384715140b7a000a81f91c8b890e57bf
andylws <andylws@naver.com>
2021-04-06 15:06:47 +0900

    solved P5 in HW7

--- HW7/P5.py
+++ HW7/P5.py
@@ -1,3 +1,15 @@
 def P5(lst):
+    cur_num = 1
+    successive_nums_max = 0
 
-    return
+    while cur_num <= len(lst):
+        successive_nums = 0
+        for i in lst:
+            if i == cur_num:
+                successive_nums += 1
+                cur_num += 1
+        if successive_nums > successive_nums_max:
+            successive_nums_max = successive_nums
+
+    min_act = len(lst) - successive_nums_max
+    return min_act
