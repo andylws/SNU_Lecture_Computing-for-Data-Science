41a31be20656339d977055bbd726d8231ba01cae
andylws <andylws@naver.com>
2021-04-07 18:53:19 +0900

    solved P1 in proj2

--- Proj2/P1.py
+++ Proj2/P1.py
@@ -3,10 +3,31 @@
 Please see instruction document.
 
 """
-def P1(path: str) -> str:        
+
+
+def P1(path: str) -> str:
     ##### Write your Code Here #####
+    path_list = []
+    cur_dir = ''
 
-    return ''
+    for char in path:
+        if char != '/':
+            cur_dir = cur_dir + char
+        else:
+            if cur_dir == '':
+                continue
+            elif cur_dir == '.':
+                continue
+            elif cur_dir == '..':
+                if path_list == []:
+                    continue
+                else:
+                    path_list.pop()
+            else:
+                path_list.append(cur_dir)
+            cur_dir = ''
+
+    result_path = '/' + '/'.join(path_list)
+
+    return result_path
     ##### End of your code #####
-
-
