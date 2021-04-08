33267a42fbbf37c2ac5a7d907777f106881811fc
andylws <andylws@naver.com>
2021-04-08 20:16:28 +0900

    solved P2 in proj2

--- Proj2/P2.py
+++ Proj2/P2.py
@@ -3,10 +3,33 @@
 Please see instruction document.
 
 """
-def P2(parentheses: str) -> bool:        
+
+
+def P2(parentheses: str) -> bool:
     ##### Write your Code Here #####
+    par_list = ['.']
 
-    return False
+    for i in parentheses:
+        if i == '(' or i == '{' or i == '[':
+            par_list.append(i)
+        elif i == ')':
+            if par_list[-1] == '(':
+                par_list.pop()
+            else:
+                return False
+        elif i == '}':
+            if par_list[-1] == '{':
+                par_list.pop()
+            else:
+                return False
+        elif i == ']':
+            if par_list[-1] == '[':
+                par_list.pop()
+            else:
+                return False
+
+    if par_list == ['.']:
+        return True
+    else:
+        return False
     ##### End of your code #####
-
-
