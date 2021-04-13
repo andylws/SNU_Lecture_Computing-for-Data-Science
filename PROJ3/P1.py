17849f7123cce1ba73846ef7ed0f6f8e8f48745c
andylws <andylws@naver.com>
2021-04-13 19:05:57 +0900

    deleted some checking lines

--- PROJ3/P1.py
+++ PROJ3/P1.py
@@ -8,13 +8,9 @@ def P1(nums, k):
     for i in range(k):
         r_dict[i] = 0
 
-    print(r_dict)
-
     for num in nums:
         remainder = num % k
         r_dict[remainder] = r_dict[remainder] + 1
-
-    print(r_dict)
 
     if r_dict[0] % 2 == 0:
         result = True
