129b0a683681b169d1c35c588e1d332fb548221d
andylws <andylws@naver.com>
2021-04-13 19:04:42 +0900

    solved P1 in proj3
    which would be better?
    1. returning value(boolean) of variable result
    2. returning True/False directly

--- PROJ3/P1.py
+++ PROJ3/P1.py
@@ -2,6 +2,31 @@ def P1(nums, k):
     '''
     nums: list, k: int
     '''
+    r_dict = {}
+    result = None
 
-    
+    for i in range(k):
+        r_dict[i] = 0
+
+    print(r_dict)
+
+    for num in nums:
+        remainder = num % k
+        r_dict[remainder] = r_dict[remainder] + 1
+
+    print(r_dict)
+
+    if r_dict[0] % 2 == 0:
+        result = True
+        for i in range(1, k // 2 + 1):
+            if r_dict[i] != r_dict[k - i]:
+                result = False
+                return result
+            else:
+                result = True
+    else:
+        result = False
+
+    return result
+
     # return type: bool
