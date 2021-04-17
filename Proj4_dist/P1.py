d2b00e8a3b17f84f3daf777fcdd0f1b92d297865
andylws <andylws@naver.com>
2021-04-17 14:50:47 +0900

    solved P1 in proj4

--- Proj4_dist/P1.py
+++ Proj4_dist/P1.py
@@ -9,5 +9,33 @@ from BST_Helper import *
 def P1(root: TreeNode, low: int, high: int) -> int:
     ##### Write your Code Here #####
 
-    return -1
+    class rangeSum():
+        def __init__(self, root, low, high):
+            self.root = root
+            self.low = low
+            self.high = high
+            self.sum = 0
+
+        def __rangeCheckHelp(self, curNode):
+            if curNode.val > high:
+                if curNode.left != None:
+                    self.__rangeCheckHelp(curNode.left)
+            elif curNode.val < low:
+                if curNode.right != None:
+                    self.__rangeCheckHelp(curNode.right)
+            else:
+                if curNode.left != None:
+                    self.__rangeCheckHelp(curNode.left)
+                if curNode.right != None:
+                    self.__rangeCheckHelp(curNode.right)
+                self.sum = self.sum + curNode.val
+
+        def rangeCheck(self):
+            self.__rangeCheckHelp(self.root)
+            return self.sum
+
+    rangeSum = rangeSum(root, low, high)
+    result = rangeSum.rangeCheck()
+
+    return result
     ##### End of your code #####
