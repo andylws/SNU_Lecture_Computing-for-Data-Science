44811e8c4fcae7570c13c81407809cf40a347d27
andylws <andylws@naver.com>
2021-04-19 16:37:12 +0900

    modified P2&P3&P4

--- PROJ5/P2.py
+++ PROJ5/P2.py
@@ -3,6 +3,5 @@ def P2(n, edges):
     n: integer (>=1)
     edges: list of tuples, len(edges) >= 0
     '''
-   
 
     # return type: integer
--- PROJ5/P3.py
+++ PROJ5/P3.py
@@ -2,7 +2,5 @@ def P3(world):
     '''
     world: M x N list, M>=1, N>=1
     '''
-    
-    
-    # return type: integer
 
+    # return type: integer
