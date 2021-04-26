67787e306e556f16246b7ccee362a475b1e0a534
andylws <andylws@naver.com>
2021-04-26 22:58:58 +0900

    solved P1 in proj6

--- PROJ6/P1.py
+++ PROJ6/P1.py
@@ -1,6 +1,14 @@
 def P1(num):
     # num: int
-    
-    
-    
+    count = 0
+    n = num
+    while n > 0:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            count = count + 1
+            n = n // 2
+
+    return count
+
     # return type: int
