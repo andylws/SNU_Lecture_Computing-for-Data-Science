1f70f2989ecea914db184d32dd2773bc34995819
andylws <andylws@naver.com>
2021-04-28 10:39:56 +0900

    solved P4 in proj6
    how can i solve this with bit operation?

--- PROJ6/P4.py
+++ PROJ6/P4.py
@@ -1,7 +1,13 @@
 def P4(nums):
     # nums: list of int
-    
-    
-    
-    
+    result = set()
+
+    for i in nums:
+        if i not in result:
+            result.add(i)
+        else:
+            result.remove(i)
+
+    return result
+
     # return type: set
