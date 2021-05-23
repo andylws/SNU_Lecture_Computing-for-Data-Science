281acf66ca476bb5aba393248a250e9e36f25c2e
andylws <andylws@naver.com>
2021-05-23 19:49:05 +0900

    solved P5 in HW10

---
+++ HW10/P5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define LETTER '1'
+#define ZERO 0
+#define NUMBER 123
+
+int main(void)
+{
+    printf("%c", 'a');
+    printf("x%x", 12288);
+    printf("$%d.%c%d n", NUMBER, LETTER, ZERO);
+}
