420b4e2564ff3503cd984d8c5694d8ac17d33d56
andylws <andylws@naver.com>
2021-05-23 19:44:46 +0900

    sovled P3 in HW10

---
+++ HW10/P3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define STOP 0
+
+int main(void)
+{
+    int counter;
+    int startPoint;
+
+    printf("==== Countdown Program ====\n");
+    printf("Enter a positive integer: ");
+    scanf("%d", &startPoint);
+
+    for (counter = startPoint; counter >= STOP; counter--)
+    {
+        printf("%d\n%d\n", counter, startPoint + counter);
+    }
+}
