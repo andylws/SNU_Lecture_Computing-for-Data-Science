eb9742b842f5371f629abf28581eb8578834db83
andylws <andylws@naver.com>
2021-05-30 18:06:33 +0900

    solved P2 in HW12

--- HW12/P2.c
+++ HW12/P2.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int P2(int n);
 
-// Implement P2 
+// Implement P2
 // You can define other function, but P2 must return answer.
-int P2(int n){
+int P2(int n)
+{
 
+    bool done = false;
+    int result = 0;
 
-    return 0;
+    while (!done)
+    {
+        done = true;
+        result++;
+        for (int i = 1; i <= n; i++)
+        {
+            if (result % i != 0)
+            {
+                done = false;
+                break;
+            }
+        }
+    }
+
+    return result;
 }
 
-
 // DO NOT MODIFY BELOW!
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
     int n = atoi(argv[1]);
     int ans = P2(n);
 
