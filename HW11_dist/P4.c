b45d8794ad108089712b64dc8e874917a0284a5d
andylws <andylws@naver.com>
2021-05-30 12:39:17 +0900

    solved P4 in HW11

--- HW11_dist/P4.c
+++ HW11_dist/P4.c
@@ -4,30 +4,40 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Please write your code below */	
-int base_four(int n){
+/* Please write your code below */
+int base_four(int n)
+{
 
+	if (n < 4)
+	{
+		return n;
+	}
 
-	return 0;
+	int remain = n % 4;
+
+	return base_four(n / 4) * 10 + remain;
 }
 /* Do not modify below */
 
-void main(int argc, char* argv[]){
+void main(int argc, char *argv[])
+{
 
-	if (argc != 2){
+	if (argc != 2)
+	{
 		printf("Enter any natural number n\n");
 	}
 
-	int n = atoi(argv[1]);		
+	int n = atoi(argv[1]);
 
-	if (n < 0){
+	if (n < 0)
+	{
 		printf("No negative number is allowed\n");
 	}
 
 	int f = 0;
 
 	f = base_four(n);
-	printf("%d\n",f);	
+	printf("%d\n", f);
 }
-
