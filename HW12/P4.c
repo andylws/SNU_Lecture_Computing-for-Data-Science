1e12ab8d2ea65be80113460e6ff6e58046ce0af5
andylws <andylws@naver.com>
2021-05-30 19:20:57 +0900

    solved P4 in HW12

--- HW12/P4.c
+++ HW12/P4.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int P4(int n);
+int sumIndivDigits(int input_i);
 
-// Implement P4 
+// Implement P4
 // You can define other function, but P4 must return answer.
-int P4(int n){
-
-
-    return 0;
+int sumIndivDigits(int input_i)
+{
+    if (input_i < 10)
+    {
+        return input_i;
+    }
+    return sumIndivDigits(input_i / 10) + (input_i % 10);
 }
 
+int P4(int n)
+{
+
+    bool self_num = false;
+
+    while (!self_num)
+    {
+        self_num = true;
+        n++;
+        for (int i = 1; i < n; i++)
+        {
+            int sum_indiv_digits = sumIndivDigits(i);
+            if (i + sum_indiv_digits == n)
+            {
+                self_num = false;
+                break;
+            }
+        }
+    }
+
+    return n;
+}
 
 // DO NOT MODIFY BELOW!
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
     int n = atoi(argv[1]);
     int ans = P4(n);
 
