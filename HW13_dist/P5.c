ee93686a79b5aa1f9d6f16e1d608e38eb39cbbf1
andylws <andylws@naver.com>
2021-06-05 01:14:03 +0900

    solved P5 in HW13

--- HW13_dist/P5.c
+++ HW13_dist/P5.c
@@ -3,13 +3,29 @@
 #define MAX_LEN 10
 
 char *SwitchCase(char *s);
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
   char *str; //List of characters to be sorted
 
   str = argv[1];
   printf("%s", SwitchCase(str));
-
 }
 /* Do not modify above */
 /* Write your code below */
+char *SwitchCase(char *s)
+{
+  char *cur = s;
+  while (*cur != '\0')
+  {
+    if (*cur > 96)
+    {
+      *cur = *cur - 32;
+    }
+    else
+    {
+      *cur = *cur + 32;
+    }
+    cur = cur + 1;
+  }
+  return s;
+}
