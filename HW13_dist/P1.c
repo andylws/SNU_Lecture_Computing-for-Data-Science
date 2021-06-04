12b7a6e57c7dbb3adbcdbecc8eefd76774b92b4c
andylws <andylws@naver.com>
2021-06-04 20:39:40 +0900

    solved P1 in HW13

--- HW13_dist/P1.c
+++ HW13_dist/P1.c
@@ -3,27 +3,31 @@
 
 /* Correct below function to swap two characters pointed by x and y 
   Then return the first pointer */
-char* swap_pointers(char *x, char *y)
+char *swap_pointers(char *x, char *y)
 {
-  /* Write your code here */	
-  
-	/* Do no modify below */	
+  /* Write your code here */
+  char temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
+  return x;
+  /* Do no modify below */
 }
 
-
-int main(int argc, char* argv[])
-{  
-  if (argc != 3){
+int main(int argc, char *argv[])
+{
+  if (argc != 3)
+  {
     printf("Please input 2 characters\n");
     return -1;
-  }     
+  }
 
-  char* s1 = argv[1];
-  char* s2 = argv[2];   
+  char *s1 = argv[1];
+  char *s2 = argv[2];
 
-  char* t1;  
-  
-  t1 = swap_pointers(s1,s2);  
-  printf("%s,%s,%s\n", s1,s2,t1);  
+  char *t1;
+
+  t1 = swap_pointers(s1, s2);
+  printf("%s,%s,%s\n", s1, s2, t1);
   return 0;
 }
