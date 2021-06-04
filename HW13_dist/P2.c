6c2f6033ed30f6a360091d71edb9c6cf6777a8fe
andylws <andylws@naver.com>
2021-06-04 22:23:06 +0900

    solved P2 in HW13

--- HW13_dist/P2.c
+++ HW13_dist/P2.c
@@ -7,23 +7,25 @@
 void printElement(char **ptr, int i)
 {
 	char *ptr1;
-	/* Write your code here */	
+	/* Write your code here */
+	ptr1 = ptr[i];
 
-
-	/* Do no modify below */	
+	/* Do no modify below */
 	printf("%s\n", ptr1);
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	char *arr[] = { "ant", "bat", "cat", "dog", "egg", "fly", "man", "god", "ptr", "str" };
-	if (argc < 2){
+	char *arr[] = {"ant", "bat", "cat", "dog", "egg", "fly", "man", "god", "ptr", "str"};
+	if (argc < 2)
+	{
 		printf("Please input any integer as argument\n");
 		return -1;
 	}
-	
+
 	int i = atoi(argv[1]);
-	if (i > 9 || i < 0){
+	if (i > 9 || i < 0)
+	{
 		printf("Please input integer between 0 and 9(inclusive)");
 		return -1;
 	}
