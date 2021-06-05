8c50d6cc048c31453205b04ccbf445e159302dc9
andylws <andylws@naver.com>
2021-06-06 01:49:20 +0900

    solved P3 in HW14
    issue in fprintf
    - why isn't it working when I start fprintf with num_str[0]?

--- HW14/P3.c
+++ HW14/P3.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){    
-    char *input_filename = argv[1]; // name of input file
+int main(int argc, char *argv[])
+{
+    char *input_filename = argv[1];  // name of input file
     char *output_filename = argv[2]; // name of output file
     // YOUR CODE HERE
+    FILE *inputFilePtr;
+    FILE *outputFilePtr;
+    char num_str[11];
+    char num_char;
 
-
-    return 0;
+    inputFilePtr = fopen(input_filename, "r");
+    outputFilePtr = fopen(output_filename, "w");
+    while (fscanf(inputFilePtr, "%s", num_str) != EOF)
+    {
+        num_char = num_str[0];
+        fprintf(outputFilePtr, "%c", num_char);
+        for (int i = 1; i < 3; i++)
+        {
+            fprintf(outputFilePtr, "%c", num_str[i]);
+        }
+        fprintf(outputFilePtr, "-");
+        for (int i = 3; i < 7; i++)
+        {
+            fprintf(outputFilePtr, "%c", num_str[i]);
+        }
+        fprintf(outputFilePtr, "-");
+        for (int i = 7; i < 11; i++)
+        {
+            fprintf(outputFilePtr, "%c", num_str[i]);
+        }
+        fprintf(outputFilePtr, "\n");
+    }
+    fclose(inputFilePtr);
+    fclose(outputFilePtr);
 }
