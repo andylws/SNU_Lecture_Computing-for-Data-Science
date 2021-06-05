050e87e57f5010e052b8373902f271c3bdcc52bb
andylws <andylws@naver.com>
2021-06-05 19:50:24 +0900

    solved P1 in HW14

--- HW14/P1.c
+++ HW14/P1.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     int N = atoi(argv[1]);
     // YOUR CODE HERE
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j1 = 0; j1 < N - i - 1; j1++)
+        {
+            printf(" ");
+        }
+        for (int j2 = 0; j2 < 2 * i + 1; j2++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 
     return 0;
 }
