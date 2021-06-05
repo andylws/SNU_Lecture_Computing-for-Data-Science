b759b56e106c0d8a9da868e66ebfec07e184a16f
andylws <andylws@naver.com>
2021-06-05 20:41:40 +0900

    solved P2 in HW14

--- HW14/P2.c
+++ HW14/P2.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     char *input_filename = argv[1]; // name of input file
     // YOUR CODE HERE
+    FILE *inputFilePtr;
+    int sum = 0;
+    int cur = 0;
 
+    inputFilePtr = fopen(input_filename, "r");
+    while (fscanf(inputFilePtr, "%d", &cur) != EOF)
+    {
+        sum = sum + cur;
+    }
+
+    printf("%d\n", sum);
 
     return 0;
 }
