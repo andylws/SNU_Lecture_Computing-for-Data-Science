89699a2711b5229136adb928a21e4b19686574e0
andylws <andylws@naver.com>
2021-06-06 02:23:45 +0900

    solved P4 in HW14

--- HW14/P4.c
+++ HW14/P4.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){    
-    char *input_filename = argv[1]; // name of input file
+int main(int argc, char *argv[])
+{
+    char *input_filename = argv[1];  // name of input file
     char *output_filename = argv[2]; // name of output file
     // YOUR CODE HERE
+    FILE *inputFilePtr;
+    FILE *outputFilePtr;
+    int num = 0;
+
+    inputFilePtr = fopen(input_filename, "r");
+    outputFilePtr = fopen(output_filename, "w");
+
+    while (fscanf(inputFilePtr, "%d", &num) != EOF)
+    {
+        fprintf(outputFilePtr, "%d = ", num);
+        for (int i = 2; i <= num; i++)
+        {
+            while (num % i == 0)
+            {
+                if (num / i == 1)
+                {
+                    fprintf(outputFilePtr, "%d", i);
+                    break;
+                }
+                num = num / i;
+                fprintf(outputFilePtr, "%d * ", i);
+            }
+        }
+        fprintf(outputFilePtr, "\n");
+    }
 
     return 0;
 }
