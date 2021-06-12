878472bff8060aac7eea46b89a530a3645127e69
andylws <andylws@naver.com>
2021-06-12 18:00:44 +0900

    solved P2 in Proj7

--- Proj7_dist/P2.c
+++ Proj7_dist/P2.c
@@ -1,53 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-  Print reversed linked list
-*/
+
 // A structure to represent a stack
 struct Stack {
-    int top;
-    unsigned capacity;
-    char* array;
+  int top;
+  unsigned capacity;
+  char* array;
 };
  
 // function to create a stack of given capacity. It initializes size of
 // stack as 0
 struct Stack* createStack(unsigned capacity)
 {
-    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
-    stack->capacity = capacity;
-    stack->top = -1;
-    stack->array = (char*)malloc(stack->capacity * sizeof(char));
-    return stack;
+  struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+  stack->capacity = capacity;
+  stack->top = -1;
+  stack->array = (char*)malloc(stack->capacity * sizeof(char));
+  return stack;
 }
  
 // Stack is full when top is equal to the last index
 int isFull(struct Stack* stack)
 {
-    return stack->top == stack->capacity - 1;
+  return stack->top == stack->capacity - 1;
 }
  
 // Stack is empty when top is equal to -1
 int isEmpty(struct Stack* stack)
 {
-    return stack->top == -1;
+  return stack->top == -1;
 }
  
 // Function to add an item to stack.  It increases top by 1
 void push(struct Stack* stack, char item)
 {
-    if (isFull(stack))
-        return;
-    stack->array[++stack->top] = item;
+  if (isFull(stack))
+    return;
+  stack->array[++stack->top] = item;
 }
  
 // Function to remove an item from stack.  It decreases top by 1
 char pop(struct Stack* stack)
 {
-    if (isEmpty(stack))
-        return '0';
-    return stack->array[stack->top--];
+  if (isEmpty(stack))
+    return '0';
+  return stack->array[stack->top--];
 }
 
 int main(int argc, char* argv[])
@@ -58,15 +56,72 @@ int main(int argc, char* argv[])
 
   /* Write your code here */	
 
+  struct Stack* stack = createStack(10);
+  int i = 0;
+
+  while(strInput[i] != '\0') {
+    if (strInput[i] == '(' || strInput[i] == '{' || strInput[i] == '['){
+      push(stack, strInput[i]);
+    }
+    else if (strInput[i] == ')') {
+      if (isEmpty(stack)) {
+        success = -1;
+      }
+      else if (stack->array[stack->top] == '(') {
+        pop(stack);
+      } 
+      else {
+        success = -1;
+        break;
+      }
+    }
+    else if (strInput[i] == '}') {
+      if (isEmpty(stack)) {
+        success = -1;
+      }
+      else if (stack->array[stack->top] == '{') {
+        pop(stack);
+      } 
+      else {
+        success = -1;
+        break;
+      }
+    }
+    else if (strInput[i] == ']') {
+      if (isEmpty(stack)) {
+        success = -1;
+      }
+      else if (stack->array[stack->top] == '[') {
+        pop(stack);
+      } 
+      else {
+        success = -1;
+        break;
+      }
+    }
+    else {
+        printf("Wrong input!");
+    }
+    if (isFull(stack)) {
+      struct Stack* newStack = createStack(2 * stack->capacity);
+      for (int j = 0; j < stack->capacity; j++) {
+        push(newStack, stack->array[j]);
+      }
+      stack = newStack;
+      free(newStack);
+    }
+    i++;
+  }
 
 
+  if (!isEmpty(stack)) {
+    success = -1;
+  }
 
-	/* Do not modify below */	
+  /* Do not modify below */	
   printf("%d", success);
 
   return 0;
-
-
 }
 
 
